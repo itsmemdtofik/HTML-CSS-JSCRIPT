52828deeeed426346dba3ba6c5161047a415e469
taufique siddiqui <63937933+tofiksiddiqui@users.noreply.github.com>
2021-01-18 16:46:40 +0530

    Add files via upload

---
+++ PROJECTS/JSCRIPTANIMATIONPROJECT/js/animation.js
@@ -0,0 +1,64 @@
+function fun1() {
+            document.getElementById('img').style.filter = "Blur(2px)";
+        }
+
+        function fun2() {
+            document.getElementById('img').style.filter = "Brightness(200%)";
+        }
+
+        function fun3() {
+            document.getElementById('img').style.filter = "Opacity(30%)";
+        }
+
+        function fun4() {
+            document.getElementById('img').style.filter = "Contrast(200%)";
+        }
+
+        function fun5() {
+            document.getElementById('demo1').style.backgroundColor = "deeppink";
+            document.getElementById('demo1').style.boxShadow = "8px 8px 10px black";
+            document.getElementById('demo1').style.borderRadius = "100px";
+            document.getElementById('img').style.borderRadius = "100px";
+            document.getElementById('img').style.width = "100px";
+            document.getElementById('img').style.height = "100px";
+            document.getElementById('button1').style.borderRadius = "100px";
+            document.getElementById('button2').style.borderRadius = "100px";
+            document.getElementById('button3').style.borderRadius = "100px";
+            document.getElementById('button4').style.borderRadius = "100px";
+            document.getElementById('button5').style.borderRadius = "100px";
+            document.getElementById('button6').style.borderRadius = "100px";
+            document.getElementById('button7').style.borderRadius = "100px";
+            document.getElementById('button8').style.borderRadius = "100px";
+            document.getElementById('button9').style.borderRadius = "100px";
+            document.getElementById('button10').style.borderRadius = "100px";
+            document.getElementById('button10').style.backgroundColor = "black";
+            document.getElementById('button10').style.color = "white";
+            document.getElementById('button11').style.borderRadius = "100px";
+
+
+        }
+
+        function fun6() {
+            document.getElementById('img').style.filter = "drop-shadow(8px 8px 10px gray)";
+
+        }
+
+        function fun7() {
+            document.getElementById('img').style.filter = "grayscale(200%)";
+
+        }
+
+        function fun8() {
+            document.getElementById('img').style.filter = "invert(60%)";
+
+        }
+
+        function fun9() {
+            document.getElementById('img').style.filter = "saturate(10)";
+
+        }
+
+        function fun10() {
+            document.getElementById('img').style.filter = "sepia(160%)";
+
+        }
